aacdd9982c47524ec697cbd8a1e00eea8f20c944
xuanmai27 <xuanmai2742004@gmail.com>
2025-02-27 20:58:52 +0700

    27/2/2025

---
+++ QLNLD/Controllers/AdminHomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLNLD.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        // GET: AdminHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult QuanLy() {
+            return View();
+        }
+        public ActionResult SinhVien()
+        {
+            return View();
+        }
+        public ActionResult EditQuanLy()
+        {
+            return View();  
+        }
+        public ActionResult EditSinhVien()
+        {
+            return View();
+        }
+
+    }
+}
